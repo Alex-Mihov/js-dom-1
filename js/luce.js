8c01dbff45ff44a91dace1a4ac110f706a35211e
Alex Mihov <alexmihov.as@gmail.com>
2025-01-10 15:18:28 +0100

    feat esercizio

--- js/luce.js
+++ js/luce.js
@@ -1,1 +1,17 @@
-// 
+// dichiarazione variabili
+// variabile img
+const luce = document.getElementById("luce")
+// variabile bottone
+const bottone = document.querySelector(".bottone")
+
+console.log(luce)
+
+// quando clicco sul bottone la lampadina si accende
+bottone.addEventListener("click", luceAccesa)
+
+
+// Funzioni
+function luceAccesa() {
+    return luce.src= "./img/yellow_lamp.png"
+}
+
